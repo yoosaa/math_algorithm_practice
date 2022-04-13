a4849626dbb3786fbc5b91479bd7c449e5ed19c1
y-sano <mmyy_125115@gmail.com>
2022-04-13 21:49:33 +0900

    #10,11追加

--- answers.py
+++ answers.py
@@ -37,3 +37,22 @@ for i in range(1, int(input_line[0])+1):
     if (i + j) <= int(input_line[1]):
       total += 1
 print(total)
+
+#9
+
+#10
+total = 1
+for i in range(1, int(input())+1):
+  total *= i
+print(total)
+
+#11
+import math
+
+for i in range(2, int(input())+1):
+  for j in range(2, i):
+    if i % j == 0:
+      break
+  # breakしなかった（iがjで一度も割り切れなかった(＝素数)とき出力）
+  else:
+    print(i, end=' ')
