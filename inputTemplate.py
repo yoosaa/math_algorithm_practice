848bbe41c945f7c20a88183572307b9c24aab2ca
YoshitakaSano <sano.yoshitaka@zero-spec.com>
2023-06-03 21:00:21 +0900

    入力テンプレート追加

---
+++ inputTemplate.py
@@ -0,0 +1,15 @@
+# 数字が１つ
+# 入力：N
+N = int(input())
+
+# 数字が２つ以上で別々に受け取り
+# 入力：A,B
+A, B = map(int, input().split())
+
+# 文字列が１つ
+# 入力：S
+S = input()
+
+# リストで受け取り
+# 入力：A1, A2, ....An
+A = list(map(int, input().split()))
